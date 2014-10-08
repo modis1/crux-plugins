a98802347230813c8c15404566e03ee814457c98
samuel@cruxframework.org <samuel@cruxframework.org@e1a744db-34b1-0808-4b55-197acf403b58>
2014-10-08 15:14:28 +0000

    IGNORE adding gwt 2.6.1 compatibility

--- crux-gadgets/src/main/java/org/cruxframework/crux/plugin/gadget/linker/GadgetLinker.java
+++ crux-gadgets/src/main/java/org/cruxframework/crux/plugin/gadget/linker/GadgetLinker.java
@@ -46,14 +46,14 @@ public final class GadgetLinker extends CrossSiteIframeLinker
 {
 	private static final String SCRIPT_SRC_HANGOUTSAPI_SANDBOX = "<script src=\"//hangoutsapi.talkgadget.google.com/hangouts/_/api/hangout.js?v=1.3\" type=\"text/javascript\"></script>";
 	private static final String SCRIPT_SRC_HANGOUTSAPI = "<script src=\"//talkgadget.google.com/hangouts/_/api/hangout.js?v=1.3\" type=\"text/javascript\"></script>";
-	private static final String GADGET_LINKER_TEMPLATE_JS = "org/cruxframework/crux/gadget/linker/GadgetTemplate.js";
-	private static final String GADGET_COMPUTE_SCRIPT_BASE_JS = "org/cruxframework/crux/gadget/linker/computeScriptBase.js";
-	private static final String GADGET_INSTALL_SCRIPT_JS = "org/cruxframework/crux/gadget/linker/installScriptEarlyDownload.js";
-	private static final String GADGET_PROCESS_METAS_JS = "org/cruxframework/crux/gadget/linker/processMetas.js";
-	private static final String GADGET_WAIT_FOR_BODY_LOADED_JS = "org/cruxframework/crux/gadget/linker/waitForBodyLoaded.js";
-	private static final String GADGET_SET_LOCALE_JS = "org/cruxframework/crux/gadget/linker/setGadgetLocale.js";
-	private static final String GADGET_COMPUT_URL_FOR_RESOURCE_JS = "org/cruxframework/crux/gadget/linker/computeUrlForGadgetResource.js";
-	private static final String HANGOUT_GADGET_COMPUT_URL_FOR_RESOURCE_JS = "org/cruxframework/crux/gadget/linker/computeUrlForHangoutGadgetResource.js";
+	private static final String GADGET_LINKER_TEMPLATE_JS = "org/cruxframework/crux/plugin/gadget/linker/GadgetTemplate.js";
+	private static final String GADGET_COMPUTE_SCRIPT_BASE_JS = "org/cruxframework/crux/plugin/gadget/linker/computeScriptBase.js";
+	private static final String GADGET_INSTALL_SCRIPT_JS = "org/cruxframework/crux/plugin/gadget/linker/installScriptEarlyDownload.js";
+	private static final String GADGET_PROCESS_METAS_JS = "org/cruxframework/crux/plugin/gadget/linker/processMetas.js";
+	private static final String GADGET_WAIT_FOR_BODY_LOADED_JS = "org/cruxframework/crux/plugin/gadget/linker/waitForBodyLoaded.js";
+	private static final String GADGET_SET_LOCALE_JS = "org/cruxframework/crux/plugin/gadget/linker/setGadgetLocale.js";
+	private static final String GADGET_COMPUT_URL_FOR_RESOURCE_JS = "org/cruxframework/crux/plugin/gadget/linker/computeUrlForGadgetResource.js";
+	private static final String HANGOUT_GADGET_COMPUT_URL_FOR_RESOURCE_JS = "org/cruxframework/crux/plugin/gadget/linker/computeUrlForHangoutGadgetResource.js";
 
 	private ArtifactSet toLink;
 
